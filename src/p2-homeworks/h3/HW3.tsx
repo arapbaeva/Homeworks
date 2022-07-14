d2295b5d1bbaefc98ca76449f92e11b36c0c9137
Пользователь <barataiym.arapbaeva.kk@gmail.com>
2022-07-14 19:44:03 +0600

    added HW3

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,20 +1,27 @@
 import React, {useState} from 'react'
 import GreetingContainer from './GreetingContainer'
 import s from './Greeting.module.css'
+import {v1} from "uuid";
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string
+    name: string
 }
+
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([
+        {_id: v1(), name: "Programmer"}
+    ])
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+
+    const addUserCallback = (name: string) => {
+        const newUser = {_id: v1(), name: name}
+        setUsers([...users, newUser])
     }
+
 
     return (
         <div>
@@ -24,7 +31,11 @@ function HW3() {
 
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer users={users}
+                               addUserCallback={addUserCallback}
+
+            />
+            <div>{users.map((user) => <div>{user.name}</div>)}</div>
 
 
             {/*для личного творчества, могу проверить*/}
